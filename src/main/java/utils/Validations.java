adba6fb59524619b5fd84b4c6b20d24819f3f3f9
Unknown <msuwanchodratsamee@gmail.com>
2018-02-03 20:59:57 +0700

    Finished isValidRequest method

--- src/main/java/utils/Validations.java
+++ src/main/java/utils/Validations.java
@@ -1,18 +1,25 @@
 package utils;
 
-import fj.data.Array;
-
-import static fj.data.Array.array;
+import java.util.function.Function;
 
 public class Validations {
     public static Boolean isValidRequest(Long cash) {
-        final Array<Long> listValidValue = array(
-            Constants.TWENTY,
-            Constants.FIFTY,
-            Constants.HUNDRED,
-            Constants.FIVE_HUNDRED,
-            Constants.THOUSAND);
+        Function<Long[], Long> helper = notesList -> {
+            Boolean flag = true;
+            Long remaining = cash;
 
-        return !listValidValue.forall(validValue -> (cash % validValue) != 0);
+            for(int i = 0; i < 2 && flag; i++) {
+                remaining = remaining % notesList[i];
+
+                if(remaining == 0) flag = false;
+            }
+
+            return remaining;
+        };
+
+        Long[] notesList = { Constants.TWENTY, Constants.FIFTY };
+        Long[] notesList2 = { Constants.FIFTY, Constants.TWENTY };
+
+        return helper.apply(notesList) == 0 || helper.apply(notesList2) == 0;
     }
 }
