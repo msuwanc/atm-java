8011ffe2ab728881b305c4772030d2061a4b0cae
Unknown <msuwanchodratsamee@gmail.com>
2018-02-05 22:03:22 +0700

    Amended Configurations helper class to use static variable instead

--- src/main/java/utils/Configurations.java
+++ src/main/java/utils/Configurations.java
@@ -4,24 +4,21 @@ import com.typesafe.config.Config;
 import com.typesafe.config.ConfigFactory;
 
 public class Configurations {
+    static Config conf = ConfigFactory.load();
+
     public static Long getTwentyNotes() {
-        Config conf = ConfigFactory.load();
         return conf.getLong("notes.twenty");
     }
     public static Long getFiftyNotes() {
-        Config conf = ConfigFactory.load();
         return conf.getLong("notes.fifty");
     }
     public static Long getHundredNotes() {
-        Config conf = ConfigFactory.load();
         return conf.getLong("notes.hundred");
     }
     public static Long getFiveHundredNotes() {
-        Config conf = ConfigFactory.load();
         return conf.getLong("notes.five_hundred");
     }
     public static Long getThousandNotes() {
-        Config conf = ConfigFactory.load();
         return conf.getLong("notes.thousand");
     }
 }
